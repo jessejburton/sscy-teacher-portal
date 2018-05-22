933fe11110a5b84c3a1c863599ae5f573a2f0d30
Jesse James Burton <jesse@burtonmediainc.com>
2018-05-22 14:36:24 -0700

    Updated connection

--- print/mysql_connect.php
+++ print/mysql_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_USER', 'saltspri_web');
+DEFINE ('DB_PASSWORD', 'Baba1272*rc');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'sscy_internal');
+DEFINE ('DB_NAME', 'saltspri_internal');
 
 $sscy_database = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
 OR die('Could not connect to MySQL ' . mysqli_connect_error());
